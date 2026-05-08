4e007a2f9229f89583512ed18f0fcecaf65186e8
Name <153858005+wcc2car@users.noreply.github.com>
2026-05-08 23:27:31 +0800

    Delete _建立執行檔.bat

--- _建立執行檔.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-rem chcp 65001 > nul
-
-
-C:\Python\Localemaster\.venv\Scripts\pyinstaller --onefile --console --icon=icon.ico Localemaster_v1.py
-
-rem C:\Python\Localemaster\.venv\Scripts\pyinstaller --onefile --console --icon=ico.ico Localemaster_v1.py
-pause
